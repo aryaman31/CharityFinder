ed428fe42cc1b745601d14b88f9b0d4404ed3886
aa3120 <aa3120@ic.ac.uk>
2021-03-27 22:19:46 +0300

    DONE

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -51,15 +51,13 @@ public class Main {
     }
 
     public static List<Question> initQuestions() {
-        Question q1 = new Question("Whats ur name?", Arrays.asList("Aryaman", "Navya"));
-        Question q2 = new Question("How old are u?", Arrays.asList("10", "11", "12", "13"));
-        return Arrays.asList(q1, q2);
+        //TODO: finish
+        return null;
     }
 
     public static List<Charity> initCharities() {
-        Charity c1 = new Charity("Ur Mom Foundation", Arrays.asList("Navya", "10", "12"));
-        Charity c2 = new Charity("Omae wa shinderou", Arrays.asList("NANI", "Navya"));
-        return Arrays.asList(c2, c1);
+        //TODO: finish
+        return null;
     }
 
     public static StringBuilder displayQuestion(Question q) {
